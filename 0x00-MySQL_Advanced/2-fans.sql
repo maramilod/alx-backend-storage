a0bce4480ab0f0ce38dbf54f618fbce59127ba69
maramilod <marammilod03@gmail.com>
2024-05-09 20:10:53 +0100

    add

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,5 @@
+-- cript that ranks country origins of bands
+-- ordered by the number of (non-unique) fans
+-- Import table dump
+curl "https://intranet.alxswe.com/rltoken/uPn947gnZLaa0FJrrAFTGQ" -s
+
