3e629ba9202212496f49d06571299bd86061ceb0
maramilod <marammilod03@gmail.com>
2024-05-14 21:02:50 +0100

    add

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -10,4 +10,4 @@ def insert_school(mongo_collection, **kwargs):
     a Python function that inserts a new document
     in a collection based on kwargs:
     """
-    return mongo_collection.insert(kwargs).inserted_id
+    return mongo_collection.insert_one(kwargs).inserted_id
