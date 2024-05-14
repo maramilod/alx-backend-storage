1fb67e4afa796af2de49cbc340d8e90455bfa9a9
maramilod <marammilod03@gmail.com>
2024-05-14 21:11:02 +0100

    add

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python3
+"""
+hey
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    a Python function that changes all topics of
+    a school document based on the name:
+    """
+    return mongo_collection.update({'name':name}, {'$set': {'topics': topics}}, {multi: true})
