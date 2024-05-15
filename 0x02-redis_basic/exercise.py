09d0fa816ed21477c2efaeefc406a4ac53e39785
maramilod <marammilod03@gmail.com>
2024-05-15 19:26:47 +0100

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,7 +6,7 @@ y
 """
 import uuid
 import redis
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -20,9 +20,39 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """tore the input data in Redis using the random key and return the key."""
+        """
+        store the input data in Redis using
+        the random key and return the key.
+        """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """
+        method that take a key string argument and
+        an optional Callable argument named fn.
+        This callable will be used to convert
+        the data back to the desired format.
+        """
+        data = self._redis.get(key)
+        if data:
+            if fn:
+                return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        implement method that will automatically parametrize
+        Cache.get with str conversion function
+        """
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        implement method that will automatically parametriz
+        Cache.get with str conversion function
+        """
+        return self.get(key, lambda d: int(d))
